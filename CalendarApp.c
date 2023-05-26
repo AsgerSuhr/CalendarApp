b7459d0891bdb78ec9a26f2aaf4840347f9a6237
AsgerSuhr <asger31@gmail.com>
2023-05-26 08:52:20 +0200

    connected to the internet

--- CalendarApp.c
+++ CalendarApp.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
-
+#include "pico/cyw43_arch.h"
+#include "secrets.h"
 
 int main()
 {
     stdio_init_all();
 
+    if(cyw43_arch_init_with_country(CYW43_COUNTRY_UK)) {
+        puts("Failed to initialise\n");
+        return 1;
+    }
 
-    puts("Hello, world!");
+    puts("intialised\n");
+
+    cyw43_arch_enable_sta_mode();
+
+    if (cyw43_arch_wifi_connect_timeout_ms(SSID, PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000)) {
+        printf("failed to connect\n");
+        return 1;
+    }
+    
+    printf("connected\n");
+    
+    while(true) {
+        puts("Hello, world!");
+        sleep_ms(500);
+    }
+    
 
     return 0;
 }
